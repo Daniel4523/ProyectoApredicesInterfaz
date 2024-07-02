4ecf076accdad880ac3c1245819ccbec6083af9b
Johan Estiven Martinez Rodriguez <jestivenmartinez@ucompensar.edu.co>
2024-07-02 14:44:25 -0500

    jo

--- ProyectoApredicesInterfaz/FormRecuperacionContraseña.Designer.cs
+++ ProyectoApredicesInterfaz/FormRecuperacionContraseña.Designer.cs
@@ -59,7 +59,7 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft JhengHei UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(413, 307);
+            this.button1.Location = new System.Drawing.Point(436, 307);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(315, 48);
             this.button1.TabIndex = 8;
