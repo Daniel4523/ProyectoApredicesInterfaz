964fe4c0fdb17d8394920f0864cb68e493b159c7
Daniel Felipe Mendez Grass <danielfmendez@ucompensar.edu.co>
2024-07-02 14:32:49 -0500

    23

--- ProyectoApredicesInterfaz/FormRecuperacionContraseña.Designer.cs
+++ ProyectoApredicesInterfaz/FormRecuperacionContraseña.Designer.cs
@@ -28,80 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(436, 170);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(199, 94);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "correo";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(199, 170);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 2;
-            this.textBox2.Text = "correoConfirmacion";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(199, 306);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 22);
-            this.textBox3.TabIndex = 3;
-            this.textBox3.Text = "codigo";
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(436, 305);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // FormRecuperacionContraseña
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
             this.Name = "FormRecuperacionContraseña";
             this.Text = "FormRecuperacionContraseña";
+            this.Load += new System.EventHandler(this.FormRecuperacionContraseña_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Button button2;
     }
 }
--- ProyectoApredicesInterfaz/FormRecuperacionContraseña.cs
+++ ProyectoApredicesInterfaz/FormRecuperacionContraseña.cs
@@ -19,46 +19,15 @@ namespace ProyectoApredicesInterfaz
         public FormRecuperacionContraseña()
         {
             InitializeComponent();
-            var listTextBox = new List<TextBox> { textBox1, textBox2, textBox3 };
-            logica = new Funciones(listTextBox, null);
+          
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if (logica.ValidarEmails(textBox1.Text, textBox2.Text))
-            {
-                string codigo = logica.GenerarCodigo();
-                try
-                {
-                    logica.EnviarCorreo(textBox1.Text);
-                    MessageBox.Show("El código ha sido enviado a tu correo electrónico.");
-                    correoUsuario = textBox1.Text; 
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                }
-            }
-            else
-            {
-                MessageBox.Show("Los correos electrónicos no coinciden.");
-            }
-        }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            if (logica.VerificarCodigo(textBox3.Text))
-            {
-                MessageBox.Show("Código verificado exitosamente.");
+ 
 
-                FormConfirmacionContraseña formConfirmacion = new FormConfirmacionContraseña(correoUsuario);
-                formConfirmacion.Show();
-                this.Hide();
-            }
-            else
-            {
-                MessageBox.Show("Código incorrecto.");
-            }
+        private void FormRecuperacionContraseña_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
